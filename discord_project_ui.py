7e1cf9c6af61fd0b8d656d630fceaf865b29b913
tilnow <44685516+tilnow@users.noreply.github.com>
2021-02-06 23:09:14 +0200

    new prefix to prevent "help" problems

--- discord_project_ui.py
+++ discord_project_ui.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.reactions = True
 
-bot = commands.Bot(command_prefix='$', intents=intents)
+bot = commands.Bot(command_prefix='$$', intents=intents)
 print('bot:',bot)
 
 
