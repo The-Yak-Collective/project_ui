03d41f15c772304af92f82915bfebdf7a45f5ba5
tilnow <44685516+tilnow@users.noreply.github.com>
2021-02-14 14:43:41 +0200

    missing members intents

--- discord_project_ui.py
+++ discord_project_ui.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.reactions = True
-
+intents.members = True
 bot = commands.Bot(command_prefix='$$', intents=intents)
 print('bot:',bot)
 
