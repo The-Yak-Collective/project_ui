490a17d3246e6159b565a8ee6ebcc32681c114c4
tilnow <44685516+tilnow@users.noreply.github.com>
2022-04-29 12:03:16 +0300

    add intent to read contents

--- discord_project_ui.py
+++ discord_project_ui.py
@@ -6,6 +6,8 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.reactions = True
 intents.members = True
+intents.messages=True
+intents.message_content=True #and now also to read messages...
 bot = commands.Bot(command_prefix='$$', intents=intents)
 print('bot:',bot)
 
